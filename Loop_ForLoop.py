bde87c237e31bc26138e877bf43173391593506f
suroy <subhankarr@outlook.com>
2020-06-21 22:37:56 +0530

    Adding the loop file to the repo

---
+++ Loop_ForLoop.py
@@ -0,0 +1,8 @@
+# In this module we will discuss about the Dictionary for Python
+
+dt1={1:'Subhankar',"JH":'Romie',3:'Brain',4:'Rajesh',5:'Sammay'}
+
+for a,b in dt1.items():
+    print(a,b,end=" | ")
+
+
