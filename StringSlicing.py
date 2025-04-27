a2552c86f81e7bada9077e13016f6bd37f38e243
Subhankar Roy <>
2025-04-27 10:07:44 +0530

    Python Learnings

---
+++ StringSlicing.py
@@ -0,0 +1,13 @@
+str1="GEN AI is the most important topic to learn now a days"
+
+importantText=str1[18:28]
+print(importantText)
+
+exploreText=str1[18:0]
+print(exploreText)
+
+slice2=str1[18:len(str1)]
+print(slice2)
+
+slice3=str1[18:]
+print(slice3)
---
+++ list_Chapter.py
@@ -0,0 +1,22 @@
+list1=[1,4,6,8,3,4,5,6]
+# print(list1.sort())
+# print(list1)
+# print(list1.sort(reverse=True))
+# print(list1)
+# print(list1.__contains__(8))
+list2=['apple','poneapple','banana','watermelon','volvo']
+# print(list1.extend(list2))
+# print(list1.count)
+# print(list2.__len__())
+# copyofList2=list2.copy()
+# print(copyofList2)
+# list3=list2[1:3]
+# print(list3)
+# print(list1.index[3])
+
+for x in list2:
+    if x=="banana":
+        print('Match Found!')
+        break
+    else:
+        print("No Match Found!")
