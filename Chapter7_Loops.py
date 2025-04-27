0e6405f8d1a4cc4379ca85da8a4df1c0b662a4a3
Subhankar Roy <subhankarr@outlook.com>
2025-04-27 18:18:42 +0530

    Updated changes

---
+++ Chapter7_Loops.py
@@ -0,0 +1,20 @@
+#Use For loop to print the first 10 numbers
+# for i in range(1,100):
+#     print(i)
+
+# list1=[1,2,3,4,5,6,7,8,9,10]
+# for i in list1:
+#     print(i)
+
+#Use whiele loop to print the first 10 numbers
+i=1
+while (i<=10):
+    print(i)
+    i+=1
+#-------------------------------
+ListName=["Subhankar","Suman","Sumantra","Sushmita"]
+i=0;
+while (i<len(list(ListName))):
+    print(ListName[i])
+    i+=1
+    
--- PracticeSet_Chapter5.py
+++ PracticeSet_Chapter5.py
@@ -7,22 +7,31 @@
 
 Hindi_to_English = { "car": "गाड़ी","house"  : "घर","book": "किताब","school": "स्कूल","garden": "बगीचा" }
 
-hindiword=input("Enter the Hindi word: ")
-print("The meaning of Hindi word is: ",Hindi_to_English.get(hindiword))
+# hindiword=input("Enter the Hindi word: ")
+# print("The meaning of Hindi word is: ",Hindi_to_English.get(hindiword))
 # for i in Hindi_to_English.items():
 #     print(i)car
 #------------------------------
 
 #problem to take 5 numbers are input and print unique numbers
-numberset=set()
-n=int(input("Enter the number of elements 1: "))
-numberset.add(n)
-n=int(input("Enter the number of elements 2: "))
-numberset.add(n)
-n=int(input("Enter the number of elements 3: "))
-numberset.add(n)
-n=int(input("Enter the number of elements 4: "))
-numberset.add(n)
-n=int(input("Enter the number of elements 5: "))
-numberset.add(n)
-print(numberset)
+# numberset=set()
+# n=int(input("Enter the number of elements 1: "))
+# numberset.add(n)
+# n=int(input("Enter the number of elements 2: "))
+# numberset.add(n)
+# n=int(input("Enter the number of elements 3: "))
+# numberset.add(n)
+# n=int(input("Enter the number of elements 4: "))
+# numberset.add(n)
+# n=int(input("Enter the number of elements 5: "))
+# numberset.add(n)
+# print(numberset)
+#------------------------------
+#problem to take int and string in set
+
+globalset=set()
+numberfirst=int(input("Enter the number: "))
+globalset.add(numberfirst)
+Stringelement=input("Enter the string: ")
+globalset.add(Stringelement)
+print(globalset)
